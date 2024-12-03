56873c5e4a896a9a09b15f781971091e5f7e6f2f
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-12-03 20:17:26 +0100

    Added day3 part1.

---
+++ src/bin/day03_part1.rs
@@ -0,0 +1,29 @@
+// cargo run  --bin day03_part1
+// cargo test --bin day03_part1
+
+fn main() {
+    let input = include_str!("../././input/day03.txt");
+    let output = solve(input);
+    println!("Day03 part1: {output}");
+}
+
+fn solve(input: &str) -> i32 {
+    /*
+    println!("{total}");
+    // 2*4 + 5*5 + 11*8 + 8*5 = 161
+     */
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day03_part1() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let output = solve(input);
+        assert_eq!(output, 161)
+    }
+}
