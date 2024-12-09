189b08642358a2bb2653a2e9808bade7b3d6133e
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-12-09 21:45:31 +0100

    Day 09 part1 passing tests.

--- src/bin/day09_part1.rs
+++ src/bin/day09_part1.rs
@@ -7,13 +7,96 @@ fn main() {
     println!("Day09 part1: {output}");
 }
 
-fn solve(input: &str) -> i32 {
-    // Getting total.
-    let total: i32 = 0;
+fn parse_input(input: &str) -> Vec<Option<u32>> {
+    let mut out: Vec<Option<u32>> = Vec::new();
 
-    /*
-    println!("{total}");
-    */
+    let mut id: u32 = 0;
+    let mut iter = input.chars();
+
+    while let Some(file_size) = iter.next() {
+        let file_size = file_size.to_digit(10).unwrap();
+        for _ in 0..file_size {
+            out.push(Some(id));
+        }
+        id += 1;
+
+        if let Some(empty_length) = iter.next() {
+            let empty_length = empty_length.to_digit(10).unwrap();
+            for _ in 0..empty_length {
+                out.push(None);
+            }
+        }
+    }
+
+    out
+}
+
+fn compress_memory(files: &[Option<u32>]) -> Vec<u32> {
+    let mut out: Vec<u32> = Vec::new();
+    let mut end = files.len();
+
+    for index in 0..files.len() {
+        if index >= end {
+            return out;
+        }
+
+        if let Some(id) = files[index] {
+            out.push(id);
+            continue;
+        }
+
+        while index < end {
+            end -= 1;
+            if let Some(id) = files[end] {
+                out.push(id);
+                break;
+            }
+        }
+    }
+
+    out
+}
+
+fn compute_checksum(files: &[u32]) -> u64 {
+    files
+        .iter()
+        .enumerate()
+        .map(|(index, id)| {
+            let a = index as u64;
+            let b = *id as u64;
+            a * b
+        })
+        .sum()
+}
+/*
+fn print_memory(files: &[Option<u32>]) {
+    for id in files {
+        if let Some(id) = id {
+            print!("{id}");
+        } else {
+            print!(".");
+        }
+    }
+    println!();
+}
+
+fn print_memory_compressed(files: &[u32]) {
+    for id in files {
+        print!("{id}");
+    }
+    println!();
+}
+ */
+fn solve(input: &str) -> u64 {
+    let files = parse_input(input);
+    let compressed = compress_memory(&files);
+    let checksum: u64 = compute_checksum(&compressed);
+
+    //println!("{:?}", files);
+    //print_memory(&files);
+    //print_memory_compressed(&compressed);
+    //println!("{checksum}");
+
     /*
     2333133121414131402
 
@@ -32,7 +115,7 @@ fn solve(input: &str) -> i32 {
     0099811188827773336446555566..............
     */
 
-    total
+    checksum
 }
 
 #[cfg(test)]
