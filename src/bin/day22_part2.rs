260d7e0d057b69a3299dc20332a28256dba7a5eb
Ertingel <152289948+Ertingel@users.noreply.github.com>
2025-01-02 20:59:00 +0100

    Day22 part2 passing test.

--- src/bin/day22_part2.rs
+++ src/bin/day22_part2.rs
@@ -1,6 +1,8 @@
 // cargo run  --bin day22_part2
 // cargo test --bin day22_part2
 
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../././input/day22.txt");
     let output = solve(input, 2000);
@@ -8,6 +10,7 @@ fn main() {
 }
 
 type Number = u64;
+type Change = i8;
 
 fn parse_input(input: &str) -> Vec<Number> {
     input
@@ -30,26 +33,80 @@ fn next_number(number: Number) -> Number {
     prune(mix(number * 2048, number))
 }
 
-fn number_at_itteration(number: Number, iterations: usize) -> Number {
+fn numbers_list(number: Number, iterations: usize) -> Vec<Number> {
     let mut number = number;
+    let mut out: Vec<Number> = vec![number];
 
     for _ in 0..iterations {
         number = next_number(number);
+        out.push(number);
     }
 
-    number
+    out
 }
 
 fn solve(input: &str, iterations: usize) -> Number {
     let initial_number = parse_input(input);
-    let final_number: Vec<Number> = initial_number
+
+    let numbers_list: Vec<Vec<Number>> = initial_number
         .iter()
-        .map(|number| number_at_itteration(*number, iterations))
+        .cloned()
+        .map(|num| numbers_list(num, iterations))
         .collect();
 
-    let total: Number = final_number.iter().sum();
+    let changes: Vec<Vec<Change>> = numbers_list
+        .iter()
+        .map(|list| {
+            list.windows(2)
+                .map(|a| (a[1] % 10) as Change - (a[0] % 10) as Change)
+                .collect()
+        })
+        .collect();
 
-    total
+    let paterns: HashMap<[Change; 4], Number> = numbers_list
+        .iter()
+        .zip(&changes)
+        .map(|(numbers, changes)| {
+            let mut out: HashMap<[Change; 4], Number> = HashMap::new();
+
+            numbers[4..]
+                .iter()
+                .zip(changes.windows(4))
+                .for_each(|(quantity, changes)| {
+                    if !out.contains_key(changes) {
+                        let changes = [changes[0], changes[1], changes[2], changes[3]];
+                        out.insert(changes, quantity % 10);
+                    }
+                });
+
+            out
+        })
+        .fold(HashMap::new(), |mut a, b| {
+            for (key, val) in b {
+                if let Some(val2) = a.get(&key) {
+                    a.insert(key, val + val2);
+                } else {
+                    a.insert(key, val);
+                }
+            }
+
+            a
+        });
+
+    let max: Number = *paterns.values().max().unwrap();
+
+    /*
+    paterns
+        .iter()
+        .filter(|(_, value)| **value == max)
+        .for_each(|(key, value)| {
+            println!("{:?} {}", key, value);
+        });
+
+    println!("{:?}", paterns.get(&[-2, 1, -1, 3]));
+    */
+
+    max
 }
 
 #[cfg(test)]
@@ -59,10 +116,17 @@ mod tests {
     #[test]
     fn day22_part2_main_example() {
         let input = "1
-10
-100
+2
+3
 2024";
         let output = solve(input, 2000);
         assert_eq!(output, 23)
+    }
+
+    #[test]
+    fn day22_part2_example2() {
+        let input = "123";
+        let output = solve(input, 10);
+        assert_eq!(output, 6)
     }
 }
